de4df0878c6b97b9ea6e0ae49247f77d1d6dc1b0
VictorRpo <victtestrepo@gmail.com>
2017-09-02 18:07:47 -0500

    prueba 3

--- src/Andres.java
+++ src/Andres.java
@@ -11,5 +11,6 @@
 public class Andres {
     public static void main(String[] args) {
         System.out.print("Andres es el mejor, Armholes");
+        System.out.print("Andres es el mejor, Armholes");
     }
 }
